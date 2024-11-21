b204f2c9f9bdf78bbf33e5900d0c12c7feb768b8
niuniu <xiaoshen1024@yandex.com>
2024-11-21 10:35:05 +0800

    update custom js from pakeplus

--- src-tauri/src/extension/custom.js
+++ src-tauri/src/extension/custom.js
@@ -4,17 +4,14 @@
  * Additionally, you can directly include any script files in this file
  * that you wish to attach to the application.
  */
-
-document.addEventListener('DOMContentLoaded', () => {
-    let htmlContent = document.documentElement.innerHTML
-    htmlContent = htmlContent.replace(/target="_blank"/g, 'target="_self"')
-    document.documentElement.innerHTML = htmlContent
-    console.log('have been replaced with target="_self".')
-})
+console.log(
+    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
+    'color:orangered;font-weight:bolder'
+)
 
 document.addEventListener('DOMContentLoaded', () => {
     const originalWindowOpen = window.open
-    window.open = function (url, target, features) {
+    window.open = function (url, _, features) {
         return originalWindowOpen.call(window, url, '_self', features)
     }
     console.log('window.open has been overridden to open in the current page.')
@@ -49,3 +46,51 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     observer.observe(targetNode, config)
 })
+// custom.js
+let isSignin = localStorage.getItem('isSignin') === 'true'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const targetNode = document.body
+    // 配置观察选项
+    const config = {
+        childList: true,
+        subtree: true,
+    }
+    const observer = new MutationObserver((mutationsList, observer) => {
+        // if login, then signin
+        const loginBtn = document.querySelector(
+            'li.nav-item.auth > div > button'
+        )
+        if (loginBtn && loginBtn.innerText.includes('登录')) {
+            console.log('user nedd login')
+            return
+        } else {
+            const singleBtn = document.querySelector('.signin-btn')
+            console.log('juejin auto signin', singleBtn, isSignin)
+            if (
+                singleBtn &&
+                singleBtn.innerText.includes('去签到') &&
+                !isSignin
+            ) {
+                singleBtn.click()
+            }
+            // click
+            const signinBtn = document.querySelector(
+                'div.code-calender > button.signin'
+            )
+            console.log('juejin auto signin', signinBtn)
+            if (
+                signinBtn &&
+                signinBtn.innerText.includes('立即签到') &&
+                !isSignin
+            ) {
+                signinBtn.click()
+                isSignin = true
+                localStorage.setItem('isSignin', 'true')
+            }
+        }
+    })
+    observer.observe(targetNode, config)
+})
+
+// end custom.js
