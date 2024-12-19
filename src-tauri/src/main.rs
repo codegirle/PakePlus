1b3fb38945cead81177a06d3ee5eab951738cd9f
niuniu <xiaoshen1024@yandex.com>
2024-12-19 15:38:44 +0800

    update main rust from pakeplus

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use serde_json::Error;
+use tauri::{utils::config::WindowConfig, Menu, MenuItem, Submenu, WindowBuilder};
 
-use tauri::{Menu, MenuItem, Submenu};
-// 对command单独管理
-mod command;
+fn json_to_window_config(window_json: &str) -> Result<WindowConfig, Error> {
+    serde_json::from_str(window_json)
+}
 
 fn main() {
-    let edit_menu = Submenu::new(
+    #[cfg(any(target_os = "linux", target_os = "windows"))]
+    let menu = Menu::new();
+    #[cfg(target_os = "macos")]
+    let menu = Menu::new().add_submenu(Submenu::new(
         "Edit",
         Menu::new()
             .add_native_item(MenuItem::Undo)
@@ -17,21 +22,25 @@ fn main() {
             .add_native_item(MenuItem::SelectAll)
             .add_native_item(MenuItem::CloseWindow)
             .add_native_item(MenuItem::Quit),
-    );
+    ));
     tauri::Builder::default()
-        .menu(Menu::new().add_submenu(edit_menu))
-        .invoke_handler(tauri::generate_handler![
-            command::pakeplus::open_window,
-            command::pakeplus::preview_from_config,
-            command::pakeplus::update_build_file,
-            command::pakeplus::update_config_file,
-            command::pakeplus::update_cargo_file,
-            command::pakeplus::update_main_rust,
-            command::pakeplus::update_custom_js,
-            command::pakeplus::content_to_base64,
-            command::pakeplus::update_config_json,
-            command::pakeplus::rust_main_window,
-        ])
+        .setup(|app| {
+            let app_handle = app.handle();
+            let window_json = r#"{"label":"Tauri2","title":"jujin","url":"https://juejin.cn/","userAgent":"Mozilla/5.0 (Linux; Android 14; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Mobile Safari/537.36","width":412,"height":915,"theme":null,"resizable":true,"fullscreen":false,"maximized":false,"minWidth":400,"minHeight":300,"maxWidth":1920,"maxHeight":1080,"decorations":true,"transparent":false,"titleBarStyle":"Visible","visible":true,"focus":true,"closable":true,"minimizable":true,"maximizable":true,"alwaysOnTop":false,"alwaysOnBottom":false,"center":false,"skipTaskbar":false,"tabbingIdentifier":null,"parent":null,"dragDropEnabled":true,"browserExtensionsEnabled":false,"devtools":true,"contentProtected":false,"hiddenTitle":false,"incognito":false,"proxyUrl":null,"useHttpsScheme":false,"zoomHotkeysEnabled":false,"acceptFirstMouse":false,"additionalBrowserArgs":""}"#;
+            match json_to_window_config(window_json) {
+                Ok(config) => {
+                    println!("Parsed WindowConfig: {:?}", config);
+                    let _main_window = WindowBuilder::from_config(&app_handle, config)
+                        .build()
+                        .unwrap();
+                }
+                Err(err) => {
+                    eprintln!("Failed to parse JSON: {}", err);
+                }
+            }
+            Ok(())
+        })
+        .menu(menu)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
